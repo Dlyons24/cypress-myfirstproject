a565ea37180d201c0d390d236c665ca7655b0714
Dlyons24 <96287175+Dlyons24@users.noreply.github.com>
2025-02-11 22:31:36 -0500

    updated login/logout test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,22 @@
-
-
-describe("Login Test", () => {
-    it("should log in successfully", () => {
+describe("Login and Logout Test", () => {
+    
+    beforeEach(() => {
         cy.visit("https://ecommerce-playground.lambdatest.io/");
-        cy.findByRole("button", { name: /My account/i }).click();
-        cy.get('input[name="email"]').type('customer@practicesoftwaretesting.com'); //refactor email call from env
-        cy.get('input[name="password"]').type('Cypresstest1'); //refactor password. call from env
-        cy.get('input[type="submit"]').click();
     });
-  });
 
-describe("Login Test", () => {
-    it("should log out successfully", () => {
-        //create logout command here 
+    afterEach(() => {
+        cy.visit("https://ecommerce-playground.lambdatest.io/");
+    });
+
+    it("should log in and log out successfully", () => {
+        cy.findByRole("button", { name: /My account/i }).click();
+        cy.get('input[name="email"]').type(Cypress.env('email')); 
+        cy.get('input[name="password"]').type(Cypress.env('password'), { log: false }); // Hide password in logs
+        cy.get('input[type="submit"][value="Login"]').click();
+        cy.contains("My Account").should("be.visible"); // Verify login success (adjust according to UI feedback)
+        cy.contains('.list-group-item', 'Logout').click();
+        cy.get('h1.page-title').should('be.visible');
+
     });
 });
+
